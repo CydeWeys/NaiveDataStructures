7a8715fea449758f09a64256b11f135afa383b4e
Ben McIlwain <cydeweys@gmail.com>
2014-04-16 22:21:44 -0400

    Improves efficiency by pre-initializing the frontier

--- src/com/cydeweys/dev/naivedatastructures/ObjectGraph.java
+++ src/com/cydeweys/dev/naivedatastructures/ObjectGraph.java
@@ -1,23 +1,36 @@
 package com.cydeweys.dev.naivedatastructures;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Set;
 
 public class ObjectGraph {
 
 	public static void main(String[] args) {
-		GraphNode origin = CreateSampleGraph();
-		Set<GraphNode> minDistances = Dijkstra(origin);
+		List<GraphNode> nodes = CreateSampleGraph();
+		GraphNode origin = nodes.get(0);
+		Set<GraphNode> minDistances = Dijkstra(origin, nodes);
 		for (GraphNode node : minDistances) {
 			System.out.println(node.label + ": " + node.cost);
 		}
 	}
 	
-	public static Set<GraphNode> Dijkstra(GraphNode origin) {
+	/**
+	 * Calculates a minimum spanning tree from the origin node in a graph
+	 * @param origin The origin node
+	 * @param nodes A list of all nodes in the graph (used to initialize the frontier PriorityQueue)
+	 * @return All of the nodes in the graph with attached total path costs
+	 */
+	public static Set<GraphNode> Dijkstra(GraphNode origin, List<GraphNode> nodes) {
 		Set<GraphNode> visited = new HashSet<GraphNode>();
 		
 		PriorityQueue<GraphNode> frontier = new PriorityQueue<GraphNode>(10, new GraphNodeComparator());
+		for (int i = 1; i < nodes.size(); i++) {
+			frontier.add(nodes.get(i));
+		}
+		
 		GraphNode current = origin;
 		current.cost = 0;
 		
@@ -26,10 +39,9 @@ public class ObjectGraph {
 			for (GraphEdge candidate : current.edges) {
 				GraphNode otherEnd = candidate.otherEnd(current);
 				if (!visited.contains(otherEnd)) {
-					// TODO: frontier.contains() is not efficient.
-					if ((frontier.contains(otherEnd) && (otherEnd.cost > current.cost + candidate.cost)) || !frontier.contains(otherEnd)) {
+					if (otherEnd.cost > current.cost + candidate.cost) {
+						// TODO: It would be neat to store the path to this node as well.
 						otherEnd.cost = current.cost + candidate.cost;
-						frontier.add(otherEnd);
 					}
 				}
 			}
@@ -43,7 +55,7 @@ public class ObjectGraph {
 		return visited;
 	}
 
-	private static GraphNode CreateSampleGraph() {
+	private static List<GraphNode> CreateSampleGraph() {
 		// If this were to get any larger, it would help to have some helper to read in this data
 		// from a more standardized format.
 		GraphNode a = new GraphNode("a");
@@ -69,7 +81,9 @@ public class ObjectGraph {
 		GraphEdge hf = new GraphEdge(h, f, 3);
 		GraphEdge gi = new GraphEdge(g, i, 6);
 		GraphEdge ei = new GraphEdge(e, i, 12);
-		return a;
+		
+		List<GraphNode> nodesList = Arrays.asList(a, b, c, d, e, f, g, h, i);
+		return nodesList;
 	}
 }
 
